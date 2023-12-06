9a9eeba473e366c2b09925c06842c2d3930c1ebb
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-12-06 03:14:45 -0500

    Run create_all() for database.db

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-app = Flask(__name__)
+app = Flask(__name__, instance_path='/Users/violetedwards/Code/bison-advisor/')
 # database setup
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'bison2023'
+db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     # username can be up to 20 characters, must be entered
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable = False)
     # password can be up to 80 characters after hashing, must be entered
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable = False)
 
 @app.route('/')
 # https://youtu.be/71EU8gnZqZQ?si=Mwxt6Y7D3TGVfISr
@@ -27,6 +27,7 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
