f0f1790a17e08952fba3c40d07e9a09a313dcf44
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-12-06 01:31:16 -0500

    Add gitignore, rename main.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+app = Flask(__name__)
+# database setup
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'bison2023'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    # username can be up to 20 characters, must be entered
+    username = db.Column(db.String(20), nullable=False)
+    # password can be up to 80 characters after hashing, must be entered
+    password = db.Column(db.String(80), nullable=False)
+
+@app.route('/')
+# https://youtu.be/71EU8gnZqZQ?si=Mwxt6Y7D3TGVfISr
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
