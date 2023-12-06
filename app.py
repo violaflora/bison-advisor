b7ca03332b08ce60651faa9c6261b74ca6f81e0f
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-12-06 03:21:40 -0500

    Add register form

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
+from flask_wtf import wtforms
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+# change instance_path to the root folder on your machine
 app = Flask(__name__, instance_path='/Users/violetedwards/Code/bison-advisor/')
 # database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -11,9 +16,18 @@ db = SQLAlchemy(app)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     # username can be up to 20 characters, must be entered
-    username = db.Column(db.String(20), nullable = False)
+    username = db.Column(db.String(20), nullable = False, unique = True)
     # password can be up to 80 characters after hashing, must be entered
     password = db.Column(db.String(80), nullable = False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min = 4, max = 20)],
+        render_kw={"placeholder": "Username"})
+    password = PasswordField(
+        validators=[InputRequired(), Length(min = 4, max = 20)],
+        render_kw={"placeholder": "Password"})
+    SubmitField = SubmitField("Register")
 
 @app.route('/')
 # https://youtu.be/71EU8gnZqZQ?si=Mwxt6Y7D3TGVfISr
