df3f0ed6f5ffeb238644c13df496229bc0bd10f0
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-12-06 03:26:56 -0500

    Add sign in

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-from flask_wtf import wtforms
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 
@@ -28,6 +28,23 @@ class RegisterForm(FlaskForm):
         validators=[InputRequired(), Length(min = 4, max = 20)],
         render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "That username already exists. Please choose a different one."
+            )
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min = 4, max = 20)],
+        render_kw={"placeholder": "Username"})
+    password = PasswordField(
+        validators=[InputRequired(), Length(min = 4, max = 20)],
+        render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
 
 @app.route('/')
 # https://youtu.be/71EU8gnZqZQ?si=Mwxt6Y7D3TGVfISr
